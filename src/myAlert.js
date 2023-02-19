b33231cd00be244f2498671aa99a6c5996750ece
NguyenMong4 <ndcpham@gmail.com>
2023-02-18 18:26:38 -0800

    layout

--- src/myAlert.js
+++
@@ -1,5 +0,0 @@
-const myAlert = () => {
-    alert("hello my friend")
-}
-
-export default myAlert;
---
+++ src/pages/Following/index.js
@@ -0,0 +1,5 @@
+function Following() {
+  return <h3>following page</h3>;
+}
+
+export default Following;
